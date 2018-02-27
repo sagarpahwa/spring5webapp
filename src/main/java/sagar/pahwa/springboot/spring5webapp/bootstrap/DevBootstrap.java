72b463ec2c8f0ed8edbe682cab23e398d214e9cf
sagar <sagarpahwa25@gmail.com>
2018-02-27 05:45:58 +0530

    Added publisher.

--- src/main/java/sagar/pahwa/springboot/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/sagar/pahwa/springboot/spring5webapp/bootstrap/DevBootstrap.java
@@ -5,18 +5,22 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import sagar.pahwa.springboot.spring5webapp.model.Author;
 import sagar.pahwa.springboot.spring5webapp.model.Book;
+import sagar.pahwa.springboot.spring5webapp.model.Publisher;
 import sagar.pahwa.springboot.spring5webapp.repositories.AuthorRepository;
 import sagar.pahwa.springboot.spring5webapp.repositories.BookRepository;
+import sagar.pahwa.springboot.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -27,15 +31,19 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent>{
     public void initData(){
 
         //Dan Brown
+        Publisher publisher = new Publisher();
+        publisher.setName("ABC");
+        publisher.setAddress("USA");
+        publisherRepository.save(publisher);
         Author dan = new Author("Dan","Brown");
-        Book inferno = new Book("Inferno","1122","ABC");
+        Book inferno = new Book("Inferno","1122",publisher);
         inferno.getAuthors().add(dan);
         dan.getBooks().add(inferno);
         bookRepository.save(inferno);
         authorRepository.save(dan);
 
         //Stephen King
-        Book shining = new Book("The Shining","1123","cde");
+        Book shining = new Book("The Shining","1123",publisher);
         Author stephen = new Author("Stephen","King");
         //shining.getAuthors().add(dan);
         shining.getAuthors().add(stephen);
